45843772500ff56cfb9dc6020a6777933f150166
Fernanda Wammes <wammesfernanda@gmail.com>
2021-09-04 09:23:17 -0300

    Primeiro commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Calcular área do terreno");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("Informe a largura do terreno:");
+            int largura = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Informe o comprimento do terreno:");
+            int comprimento = Convert.ToInt32(Console.ReadLine());
+
+            calcularArea(largura, comprimento);
+        }
+        static void calcularArea (int largura, int comprimento)
+        {
+            int area = largura * comprimento;
+            Console.WriteLine (@$"Área =  { area} m²");
+
+
+            
+        }
+    }
+}
